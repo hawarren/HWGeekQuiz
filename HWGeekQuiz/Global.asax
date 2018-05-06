01f266a69af1e86b1c5c283737243119857e5e79
Hanif Warren <hanif.warren@gmail.com>
2018-05-05 20:10:13 -0400

    Add project files.

---
+++ HWGeekQuiz/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HWGeekQuiz.MvcApplication" Language="C#" %>
---
+++ HWGeekQuiz/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HWGeekQuiz.Startup))]
+namespace HWGeekQuiz
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
