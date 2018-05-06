c1699d4eeb05c872f9843ffa3b3c2d2c000d9080
Hanif Warren <hanif.warren@gmail.com>
2018-05-06 03:36:21 -0400

    End of Task 3, Steps 10 -13

--- HWGeekQuiz/Controllers/TriviaController.cs
+++ HWGeekQuiz/Controllers/TriviaController.cs
@@ -6,7 +6,7 @@ using System.Net.Http;
 using System.Web.Http;
 using System.Data.Entity;
 using System.Threading;
-using System.Threading.Tasks;;
+using System.Threading.Tasks;
 using System.Web.Http.Description;
 using HWGeekQuiz.Models;
 
@@ -19,6 +19,7 @@ namespace HWGeekQuiz.Controllers
 {
     using GeekQuiz.Models;
 
+    [Authorize]
     public class TriviaController : ApiController
     {
         private TriviaContext db = new TriviaContext();
@@ -47,7 +48,49 @@ namespace HWGeekQuiz.Controllers
 
         }
 
+        //
+        // GET api/TriviaAdd the following Get action method to the TriviaController class. This action method calls the NextQuestionAsync helper method defined in the previous step to retrieve the next question for the authenticated user.
+        [ResponseType((typeof(TriviaQuestion))]
+   public async Task<IHttpActionResult> Get()
+    {
+        var userId = User.Identity.Name;
+
+        TriviaQuestion nextQuestion = await this.NextQuestionAsync(userId);
+        if (nextQuestion == null)
+        {
+            return this.NotFound();
+        }
+        return this.Ok(nextQuestion);
     }
 
+        //Add the following helper method at the end of the TriviaController class. This method stores the specified answer in the database and returns a Boolean value indicating whether or not the answer is correct.
+        private async Task<bool> StoreAsync(TriviaAnswer answer)
+        {
+            this.db.TriviaAnswers.Add(answer);
+            await this.db.SaveChangesAsync();
+            var selectedOption =
+                await this.db.TriviaOptions.FirstOrDefaultAsync(
+                    o => o.Id == answer.OptionId && o.QuestionId == answer.QuestionId);
+            return selectedOption.IsCorrect;
+        }
+        //12. Add the following Post action method to the TriviaController class. This action method associates the answer to the authenticated user and calls the StoreAsync helper method. Then, it sends a response with the Boolean value returned by the helper method.
 
+        //POST api/Trivia
+        [ResponseType(typeof(TriviaAnswer))]
+        public async Task<IHttpActionResult> Post(TriviaAnswer answer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return this.BadRequest((this.ModelState);
+            }
+
+            answer.UserId = User.Identity.Name;
+
+            var isCorrect = await this.StoreAsync((answer));
+            return this.Ok<bool>(isCorrect);
+        }
+
+
+
+    }
 }
