7dd997dbb558b159f153848019c01be58a65e6ce
Hanif Warren <hanif.warren@gmail.com>
2018-05-05 20:32:15 -0400

    Completed all of task 1

--- HWGeekQuiz/Global.asax.cs
+++ HWGeekQuiz/Global.asax.cs
@@ -6,13 +6,19 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using HWGeekQuiz.Models;;
 
 namespace HWGeekQuiz
 {
+    using GeekQuiz.Models;
+
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
+            //sets the  TriviaDatabaseInitializer as the database initializer
+            System.Data.Entity.Database.SetInitializer(new TriviaDatabaseInitializer());
+
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
